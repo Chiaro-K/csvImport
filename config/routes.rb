c328ec19b3c599650aec37d0b11ca74f9ea121e7
khanyiso <mapumak@gmail.com>
2020-05-29 17:29:09 +0200

    Create view file to display and import csv upload - link view to controller in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'students/index'
+  # get 'students/index'
 
-  get 'students/import'
+  # get 'students/import'
+
+  resources :students do
+    collection { post :import }
+  end
+
+  root to: "students#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
