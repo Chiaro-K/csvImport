f572a52b1e359990135d42b295b2328fec229440
khanyiso <mapumak@gmail.com>
2020-06-01 23:38:39 +0200

    add css to page for better styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
